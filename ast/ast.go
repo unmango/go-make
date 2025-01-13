fee6a2191ff6eb94e00b088675793b809cc615e9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-01-13 16:21:21 -0600

    Enhanced AST with new structures and methods
    The Abstract Syntax Tree (AST) has been significantly expanded. New structures like File, TargetList, PreReqList, LiteralFileName, and Ident have been introduced to better represent the syntax of a Makefile. Each structure now includes Pos() and End() methods for determining their position within the file. The token package has also been replaced with a custom implementation to provide more flexibility in handling tokens.

--- ast/ast.go
+++ ast/ast.go
@@ -2,16 +2,21 @@ package ast
 
 import (
 	"go/ast"
-	"go/token"
+
+	"github.com/unmango/go-make/token"
 )
 
 type Node = ast.Node
 
 var Walk = ast.Walk
 
+// A File represents text content interpreted as the make syntax.
+// Most commonly this is a Makefile, but could also be any file
+// understood by make, i.e. include-me.mk
 type File struct {
 	FileStart, FileEnd token.Pos
 	Comments           []*CommentGroup
+	Rules              []*Rule
 }
 
 // A CommentGroup represents a sequence of comments with no other tokens and no empty lines between.
@@ -69,27 +74,33 @@ func (r *Rule) End() token.Pos {
 	return r.Recipes[len(r.Recipes)-1].End()
 }
 
+// A TargetList represents a list of Targets in a single Rule.
 type TargetList struct {
 	List []FileName
 }
 
+// Pos implements Node
 func (t *TargetList) Pos() token.Pos {
 	return t.List[0].Pos()
 }
 
+// End implements Node
 func (t *TargetList) End() token.Pos {
 	return t.List[len(t.List)-1].End()
 }
 
+// A PreReqList represents all normal and order-only prerequisites in a single Rule.
 type PreReqList struct {
 	Pipe token.Pos
 	List []FileName
 }
 
+// Pos implements Node
 func (p *PreReqList) Pos() token.Pos {
 	return p.List[0].Pos()
 }
 
+// End implements Node
 func (p *PreReqList) End() token.Pos {
 	return p.List[len(p.List)-1].End()
 }
@@ -97,10 +108,22 @@ func (p *PreReqList) End() token.Pos {
 // A FileName represents any Node that can appear where a file name is expected.
 type FileName interface {
 	Node
+	fileNameNode()
 }
 
-type RecipeList struct {
-	List []*Recipe
+// A LiteralFileName represents a name identifier with no additional syntax.
+type LiteralFileName struct {
+	Name *Ident
+}
+
+func (*LiteralFileName) fileNameNode() {}
+
+func (l *LiteralFileName) Pos() token.Pos {
+	return l.Name.Pos()
+}
+
+func (l *LiteralFileName) End() token.Pos {
+	return l.Name.End()
 }
 
 // A Recipe represents a line of text to be passed to the shell to build a Target.
@@ -118,4 +141,20 @@ func (r *Recipe) Pos() token.Pos {
 // End implements Node
 func (r *Recipe) End() token.Pos {
 	return token.Pos(int(r.TokPos) + len(r.Text))
+}
+
+// An Ident represents an identifier.
+type Ident struct {
+	Name    string
+	NamePos token.Pos
+}
+
+// Pos implements Node
+func (i *Ident) Pos() token.Pos {
+	return i.NamePos
+}
+
+// End implements Node
+func (i *Ident) End() token.Pos {
+	return token.Pos(int(i.NamePos) + len(i.Name))
 }
