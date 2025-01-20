2bc937c073528869dc299c285c1be297298ab80f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-01-19 23:55:27 -0600

    Flesh out the writer a little (#11)
    * Refactored write.go and tests
    
    Significant changes include:
    - Replaced strings package with reflect
    - Added ast import from unmango/go-make
    - Refactored Writer struct to use io.Writer directly
    - Updated functions to handle ast.File, ast.PreReqList, ast.Recipe, ast.Rule, and more
    - Enhanced error handling by checking for nil values before processing them
    - Updated corresponding tests in write_test.go to match the refactored code.
    
    * Enhanced Writer functionality in write.go
    
    Significant changes have been made to the Writer struct and its associated functions. The NewWriter function now checks if the provided writer is already a Writer type, returning it directly if true. Several new methods have been added to handle writing tokens, spaces, identifiers, and strings. Existing methods for writing files, prerequisite lists, recipes, rules, target lists and file names have been modified to improve their functionality. Changes also include better error handling and formatting adjustments for improved readability of output. Corresponding changes were made in write_test.go to reflect these updates.
    
    * Refactor print.go and update tests
    
    Simplified the Fprint function in print.go by extracting repetitive code into separate functions. This includes writing target lists, prerequisite lists, recipes, and rules. Also updated corresponding test cases to reflect these changes.

--- print.go
+++ print.go
@@ -1,57 +1,24 @@
 package make
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/unmango/go-make/ast"
 )
 
-func Fprint(w io.Writer, node ast.Node) (err error) {
+func Fprint(writer io.Writer, node ast.Node) (err error) {
+	w := NewWriter(writer)
 	switch node := node.(type) {
 	case *ast.LiteralFileName:
-		_, err = io.WriteString(w, node.String())
+		_, err = w.WriteIdent(node.Name)
 	case *ast.TargetList:
-		for _, t := range node.List {
-			if err = Fprint(w, t); err != nil {
-				return
-			}
-		}
-		_, err = io.WriteString(w, ":")
+		_, err = WriteTargetList(w, node)
 	case *ast.PreReqList:
-		for _, t := range node.List {
-			if err = Fprint(w, t); err != nil {
-				return
-			}
-		}
+		_, err = WritePreReqList(w, node)
 	case *ast.Recipe:
-		if _, err = fmt.Fprint(w, node.Tok); err != nil {
-			return err
-		}
-		if _, err = io.WriteString(w, node.Text); err != nil {
-			return err
-		}
-		if _, err = fmt.Fprintln(w); err != nil {
-			return err
-		}
+		_, err = WriteRecipe(w, node)
 	case *ast.Rule:
-		if err = Fprint(w, node.Targets); err != nil {
-			return err
-		}
-		if _, err = fmt.Fprint(w, " "); err != nil {
-			return err
-		}
-		if err = Fprint(w, node.PreReqs); err != nil {
-			return err
-		}
-		if _, err = fmt.Fprintln(w); err != nil {
-			return err
-		}
-		for _, r := range node.Recipes {
-			if err = Fprint(w, r); err != nil {
-				return err
-			}
-		}
+		_, err = WriteRule(w, node)
 	}
 
 	return
--- write.go
+++ write.go
@@ -3,104 +3,187 @@ package make
 import (
 	"fmt"
 	"io"
-	"strings"
+	"reflect"
+
+	"github.com/unmango/go-make/ast"
+	"github.com/unmango/go-make/token"
 )
 
 type Writer struct {
-	w io.Writer
+	io.Writer
 }
 
+// NewWriter returns a new [Writer] writing to w, or w
+// if w is already a [Writer]
 func NewWriter(w io.Writer) *Writer {
-	return &Writer{w}
+	if writer, ok := w.(*Writer); ok {
+		return writer
+	} else {
+		return &Writer{w}
+	}
 }
 
 func (w *Writer) WriteLine() (n int, err error) {
-	return io.WriteString(w.w, "\n")
+	return fmt.Fprintln(w)
 }
 
-func (w *Writer) WriteMakefile(m Makefile) (n int, err error) {
-	for _, r := range m.Rules {
-		if x, err := w.WriteRule(r); err != nil {
+func (w *Writer) WriteToken(tok token.Token) (n int, err error) {
+	return fmt.Fprint(w, tok)
+}
+
+func (w *Writer) WriteSpace() (n int, err error) {
+	return w.WriteString(" ")
+}
+
+func (w *Writer) WriteIdent(i *ast.Ident) (n int, err error) {
+	return w.WriteString(i.Name)
+}
+
+func (w *Writer) WriteString(s string) (n int, err error) {
+	return w.Write([]byte(s))
+}
+
+func WriteFile(w *Writer, f *ast.File) (n int, err error) {
+	if f == nil {
+		err = fmt.Errorf("f was nil")
+		return
+	}
+
+	for _, r := range f.Rules {
+		if c, err := WriteRule(w, r); err != nil {
 			return 0, err
 		} else {
-			n += x
+			n += c
 		}
 	}
 
 	return
 }
 
-func (w *Writer) WritePreReq(p string) (n int, err error) {
-	return io.WriteString(w.w, " "+p)
-}
+func WritePreReqList(w *Writer, l *ast.PreReqList) (n int, err error) {
+	if l == nil {
+		return
+	}
 
-func (w *Writer) WritePreReqs(ps []string) (n int, err error) {
-	for _, p := range ps {
-		if x, err := w.WritePreReq(p); err != nil {
+	for i, p := range l.List {
+		if c, err := WriteFileName(w, p); err != nil {
 			return 0, err
 		} else {
-			n += x
+			n += c
+		}
+
+		if i+1 >= len(l.List) {
+			continue
+		}
+
+		if c, err := io.WriteString(w, " "); err != nil {
+			return 0, err
+		} else {
+			n += c
 		}
 	}
 
 	return
 }
 
-func (w *Writer) WriteRecipe(r string) (n int, err error) {
-	return io.WriteString(w.w, "\t"+r)
-}
-
-func (w *Writer) WriteRecipes(rs []string) (n int, err error) {
-	for _, p := range rs {
-		if x, err := w.WriteRecipe(p); err != nil {
-			return 0, err
-		} else {
-			n += x
-		}
+func WriteRecipe(w *Writer, r *ast.Recipe) (n int, err error) {
+	if r == nil {
+		err = fmt.Errorf("r was nil")
+		return
 	}
 
-	return
+	return fmt.Fprintf(w, "%s%s\n", r.Tok, r.Text)
 }
 
-func (w *Writer) WriteRule(r Rule) (n int, err error) {
-	if len(r.Target) == 0 {
+func WriteRule(w *Writer, r *ast.Rule) (n int, err error) {
+	if r == nil {
+		err = fmt.Errorf("r was nil")
+		return
+	}
+
+	if r.Targets == nil || len(r.Targets.List) == 0 {
 		return 0, fmt.Errorf("no targets")
 	}
 
-	if n, err = w.WriteTargets(r.Target); err != nil {
-		return
+	if c, err := WriteTargetList(w, r.Targets); err != nil {
+		return 0, err
+	} else {
+		n += c
 	}
-	if x, err := w.WritePreReqs(r.PreReqs); err != nil {
+
+	if r.PreReqs != nil && len(r.PreReqs.List) > 0 {
+		if c, err := io.WriteString(w, " "); err != nil {
+			return 0, err
+		} else {
+			n += c
+		}
+	}
+
+	if x, err := WritePreReqList(w, r.PreReqs); err != nil {
 		return 0, err
 	} else {
 		n += x
 	}
+
 	if x, err := w.WriteLine(); err != nil {
 		return 0, err
 	} else {
 		n += x
 	}
-	if x, err := w.WriteRecipes(r.Recipe); err != nil {
-		return 0, err
-	} else {
-		n += x
-	}
-	if len(r.Recipe) > 0 {
-		if x, err := w.WriteLine(); err != nil {
+
+	for _, r := range r.Recipes {
+		if c, err := WriteRecipe(w, r); err != nil {
 			return 0, err
 		} else {
-			return n + x, nil
+			n += c
 		}
 	}
 
 	return
 }
 
-func (w *Writer) WriteTarget(t string) (n int, err error) {
-	return io.WriteString(w.w, t+":")
+func WriteTargetList(w *Writer, l *ast.TargetList) (n int, err error) {
+	if l == nil {
+		return
+	}
+
+	for i, t := range l.List {
+		if c, err := WriteFileName(w, t); err != nil {
+			return 0, err
+		} else {
+			n += c
+		}
+
+		if i+1 >= len(l.List) {
+			continue
+		}
+
+		if c, err := w.WriteSpace(); err != nil {
+			return 0, err
+		} else {
+			n += c
+		}
+	}
+
+	if c, err := w.WriteToken(token.COLON); err != nil {
+		return 0, err
+	} else {
+		n += c
+	}
+
+	return
 }
 
-func (w *Writer) WriteTargets(ts []string) (n int, err error) {
-	t := strings.Join(ts, " ")
-	return io.WriteString(w.w, t+":")
+func WriteFileName(w *Writer, f ast.FileName) (n int, err error) {
+	if f == nil {
+		err = fmt.Errorf("f was nil")
+		return
+	}
+
+	switch node := f.(type) {
+	case *ast.LiteralFileName:
+		return w.WriteIdent(node.Name)
+	default:
+		return 0, fmt.Errorf("unsupported node type: %s", reflect.TypeOf(f))
+	}
 }
