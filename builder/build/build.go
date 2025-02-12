0f5411dae907a239fde032ad26557e7d0f7c6d8b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-11 21:13:46 -0600

    Refactor build.go to simplify dependencies
    Removed the dependency on 'ast' package and the 'Add' method from the File interface. This simplifies the code structure and reduces unnecessary coupling, making it easier to maintain in future.

--- builder/build/build.go
+++ builder/build/build.go
@@ -1,12 +1,10 @@
 package build
 
 import (
-	"github.com/unmango/go-make/ast"
 	"github.com/unmango/go-make/token"
 )
 
 type File interface {
-	Add(ast.Obj)
 	Start(token.Pos)
 	Rule(target func(Expr), fs ...func(Rule))
 }
