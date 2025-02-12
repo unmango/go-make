a5c5b6b033c5f7362e5e47e7fb9415f7e394ea84
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-11 21:12:58 -0600

    Added new Builder function in expr package
    A new function named 'Builder' has been added to the 'expr' package. This function takes an expression from the 'ast' package and returns a function that operates on a build.Expr object. It uses a switch statement to handle different types of expressions, specifically 'Text' and 'VarRef'. The import statement was also updated to include the 'ast' package from 'go-make'.

--- builder/expr/expr.go
+++ builder/expr/expr.go
@@ -1,6 +1,7 @@
 package expr
 
 import (
+	"github.com/unmango/go-make/ast"
 	"github.com/unmango/go-make/builder/build"
 )
 
@@ -13,5 +14,16 @@ func Text(text string) func(build.Expr) {
 func VarRef(name string) func(build.Expr) {
 	return func(b build.Expr) {
 		b.VarRef(name)
+	}
+}
+
+func Builder(expr ast.Expr) func(build.Expr) {
+	return func(e build.Expr) {
+		switch n := expr.(type) {
+		case *ast.Text:
+			e.Text(n.Value)
+		case *ast.VarRef:
+			e.VarRef(n.Name)
+		}
 	}
 }
